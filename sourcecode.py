ffdea800edc27b372b605701b01d17fdb8e4c9df
Link0 <36975653+ELink0@users.noreply.github.com>
2019-08-14 20:01:42 -0300

    Update sourcecode.py

--- sourcecode.py
+++ sourcecode.py
@@ -2,6 +2,6 @@ import urllib2
 
 resposta = urllib2.urlopen("http://google.com.br")
 cod_fonte = resposta.read()
-arquivo = open("codigo.txt","w")
+arquivo = open("codigo.html","w")
 arquivo.write(cod_fonte)
 arquivo.close()
